7d55af8a368a022c10cba8c97b030a0565b845ca
Taofik Adesina <91717420+Kunlexi@users.noreply.github.com>
2023-07-28 11:32:02 +0100

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/*"use strict";
+
+module.exports = {
+  extends: ["reverentgeek/node"],
+};
+*/
+
+"use strict"
+
+{
+  "parserOptions": {
+      "ecmaVersion": "latest"
+  },
+
+  "env": {
+      "es6": true
+  }
+}
